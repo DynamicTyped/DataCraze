45e5058648549e2de965731e372622b9fce0de3e
Rick Bundren <bundy845@gmail.com>
2013-10-28 10:39:08 -0400

    test for return factory

---
+++ DataCraze.Test/TrendReturnFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCraze.Test
+{
+    [TestClass]
+    public class TrendReturnFactoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
