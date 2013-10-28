c530f3afa7518c5f16a45b4c0469b543f773b11a
Rick Bundren <bundy845@gmail.com>
2013-10-28 10:35:24 -0400

    Test for config section

---
+++ DataCraze.Test/TrendReturnSectionTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataCraze.Configuration;
+using System.Configuration;
+
+namespace DataCraze.Test
+{
+    [TestClass]
+    public class TrendReturnSectionTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            object target = new TrendReturnSection();
+            Assert.IsInstanceOfType(target, typeof(TrendReturnSection));
+        }
+
+        [TestMethod]
+        public void get_section()
+        {
+            var actual = TrendReturnSection.GetSection();
+            Assert.IsInstanceOfType(actual, typeof(TrendReturnSection));
+        }
+
+        [TestMethod]
+        public void BufferTest()
+        {
+            var target = TrendReturnSection.GetSection();
+            const double expected = .2d;
+            var actual = target.Buffer;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DataCraze/Configuration/TrendReturnSection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace DataCraze.Configuration
+{
+    public class TrendReturnSection : ConfigurationSection
+    {
+        public const string SectionName = "TrendReturn";
+
+        public static TrendReturnSection GetSection()
+        {
+            return (TrendReturnSection)ConfigurationManager.GetSection(SectionName);
+        }
+
+        private const string _bufferNameProperty = "buffer";
+        [ConfigurationProperty(_bufferNameProperty, IsRequired=false, DefaultValue=0d)]
+        public double Buffer
+        {
+            get { return (double)this[_bufferNameProperty];}
+            set { this[_bufferNameProperty] = value; }
+        }
+    }
+}
